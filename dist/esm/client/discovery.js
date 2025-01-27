5e969912686ee1c31c87e1cf743aaa4ac43b05af
Tomas Pilar <thomas7pilar@gmail.com>
2025-01-27 13:06:36 +0100

    chore: commit dist folder (#2)
    Signed-off-by: Tomas Pilar <tomas.pilar@apoco.com>

---
+++ dist/esm/client/discovery.js
@@ -0,0 +1,59 @@
+import { readFileSync } from "node:fs";
+import { z } from "zod";
+import { StdioClientTransport } from "./stdio.js";
+import { SSEClientTransport } from "./sse.js";
+const serverTransport = z.union([
+    z.object({
+        type: z.literal("stdio"),
+        command: z.string(),
+        args: z.array(z.string()),
+    }),
+    z.object({
+        type: z.literal("sse"),
+        url: z.string(),
+    }),
+]);
+const ManifestSchema = z.object({
+    servers: z.array(z.object({
+        default: z.boolean().optional(),
+        transports: z.array(serverTransport),
+    })),
+    uiServers: z
+        .array(z.object({
+        default: z.boolean().optional(),
+        transports: z.array(serverTransport),
+    }))
+        .optional(),
+});
+async function loadManifest(url) {
+    let manifestDocument;
+    switch (url.protocol) {
+        case "http:":
+        case "https:":
+            throw new Error("Not implemented");
+        case "file:":
+            manifestDocument = JSON.parse(readFileSync(url.pathname, "utf8"));
+            break;
+        default:
+            throw new Error(`Unsupported protocol ${url.protocol}`);
+    }
+    return await ManifestSchema.parseAsync(manifestDocument);
+}
+function createTransport(transport) {
+    switch (transport.type) {
+        case "stdio":
+            return new StdioClientTransport({
+                command: transport.command,
+                args: transport.args,
+            });
+        case "sse":
+            return new SSEClientTransport(new URL(transport.url));
+    }
+}
+export async function discoverServers(url) {
+    const manifest = await loadManifest(url);
+    return manifest.servers.flatMap((server) => {
+        return { ...server, transports: server.transports.map(createTransport) };
+    });
+}
+//# sourceMappingURL=discovery.js.map
